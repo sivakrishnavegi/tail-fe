8fe280e2510fd22b1cb93c675eba1581222b3357
sivakrishnavegi <sivakrishnavegi@yahoo.com>
2023-08-23 18:50:04 +0530

    added sidebar and bottombar

--- core/components/BottomBar/index.tsx
+++ core/components/BottomBar/index.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+"use client"
+import { sidebarLinks } from '@/core/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
  export const BottomBar = () => {
+  const pathname  = usePathname()
+
   return (
-    <div >BottomBar</div>
+    <section className='bottombar'>
+      <div className='bottombar_container'>
+      {sidebarLinks.map((link)=>{
+        const isActive  = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+        return(
+        <Link
+        className={`bottombar_link ${isActive && 'bg-primary-500'}`}
+         href={link.route} key={link.label}>
+          <Image
+          alt={link.label}
+           src={link.imgURL}
+           width={24}
+           height={24}
+           />
+           <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+            {link.label.split(/\s+/)[0]}</p>      
+        </Link>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
